d9207363ba01c2d91e22309ab103a9e240adc0a0
Patrick Birkle <company@it-birkle.de>
2022-05-19 22:45:35 +0200

    remove recipient from run script

--- SMART/run_test.sh
+++ SMART/run_test.sh
@@ -9,23 +9,17 @@ function check_prerequisites() {
   if [[ -z ${MODE} ]]; then
     MODE=short
   fi
-
-  if [[ -z ${MAIL_RECIPIENT} ]]; then
-    echo "no recipient provided"
-    exit 1
-  fi
 }
 
 function run_test() {
   /usr/sbin/smartctl -t "${MODE}" "${DEVICE}"
 }
 
-while getopts d:m:r: opt
+while getopts d:m: opt
 do
     case "${opt}" in
         d) DEVICE=${OPTARG};;
         m) MODE=${OPTARG};;
-        r) MAIL_RECIPIENT=${OPTARG};;
         *) echo "argument not found" && exit 1;;
     esac
 done
